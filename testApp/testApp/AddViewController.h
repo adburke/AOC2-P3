4e9fee745df03f8bfc94865f8eba5632068291af
adburke <adburke@fullsail.edu>
2013-05-21 12:16:13 -0500

    Creation of second view AddViewController

---
+++ testApp/testApp/AddViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddViewController.h
+//  testApp
+//
+//  Created by Aaron Burke on 5/21/13.
+//  Copyright (c) 2013 Aaron Burke. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface AddViewController : ViewController
+
+@end
